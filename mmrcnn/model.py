fd746e3a2082fe967bd1f4f0b2140931e9247eb4
johannathiemich <johanna.thiemich@gmx.de>
2018-06-14 13:26:25 +0200

    added big model

--- mmrcnn/model.py
+++ mmrcnn/model.py
@@ -27,6 +27,7 @@ config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 config.gpu_options.visible_device_list = "0"
 session = tf.Session(config=config)
+
 import keras
 import keras.backend as K
 import keras.layers as KL
